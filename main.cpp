bb4591ba72cf992cbb5c56b27d71d68f00d6342e
Andrii Murha <andrii.murha@aurumfoundation.pp.ua>
2023-11-01 08:18:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
 #include <fstream>
-#include <yaml-cpp/yaml.h>
+#include <sstream>
+#include <vector>
 
-/// Build project function
+// Function to split a string by space
+std::vector<std::string> split(const std::string& s) {
+    std::istringstream iss(s);
+    return {std::istream_iterator<std::string>{iss},
+            std::istream_iterator<std::string>{}};
+}
+
+// Function to build project using custom syntax
 void buildProject() {
-    std::cout << "Building project using build.amake\n";
+    std::cout << "Building project using amakefile\n";
 
-    // Read build.amake
-    std::ifstream build_file("build.amake");
+    // Read amakefile
+    std::ifstream build_file("amakefile");
     if (!build_file.is_open()) {
-        std::cerr << "Error: Couldn't open build.amake\n";
+        std::cerr << "Error: Couldn't open amakefile\n";
         return;
     }
 
-    // Parse YAML
-    YAML::Node buildConfig = YAML::Load(build_file);
-
-
-    // Extract build steps
-    const YAML::Node& steps = buildConfig["steps"];
-    if (!steps.IsSequence()) {
-        std::cerr << "Error: 'steps' must be a sequence in build.amake\n";
-        return;
+    // Parse amakefile
+    std::vector<std::string> buildConfig;
+    for (std::string line; getline(build_file, line); ) {
+        buildConfig.push_back(line);
     }
 
-    for (const auto& step : steps) {
-        std::string target = step["target"].as<std::string>();
+    for (const auto& step : buildConfig) {
+        // Split the step by space
+        std::vector<std::string> tokens = split(step);
+
+        if (tokens.empty()) {
+            continue;
+        }
+
+        std::string target = tokens[0];
         std::cout << "Building target: " << target << std::endl;
 
-        std::string type = step["type"].as<std::string>();
+        std::string type = tokens[1];
         std::cout << "Type: " << type << std::endl;
 
         std::string buildCommand;
@@ -48,95 +58,10 @@ void buildProject() {
             return;
         }
 
-        // Assuming "sources" is a sequence of strings
-        const YAML::Node& sources = step["sources"];
-        if (!sources.IsSequence()) {
-            std::cerr << "Error: 'sources' must be a sequence in build.amake\n";
-            return;
+        // Assuming the rest of the tokens are sources and libraries
+        for (size_t i = 2; i < tokens.size(); ++i) {
+            buildCommand += " " + tokens[i];
         }
-
-        const YAML::Node& linkWithTargets = step["link_with"];
-        if (linkWithTargets) {
-            for (const auto& linkTarget : linkWithTargets) {
-                std::string linkTargetName = linkTarget.as<std::string>();
-                std::cout << "Linking with: " << linkTargetName << std::endl;
-
-                // Assuming the target exists in the steps
-                const YAML::Node& linkedStep = buildConfig[linkTargetName];
-                if (!linkedStep) {
-                    std::cerr << "Error: Target '" << linkTargetName << "' not found for linking\n";
-                    return;
-                }
-
-                std::string linkCommand = "g++ -o " + target;
-
-                const YAML::Node& linkedSources = linkedStep["sources"];
-                for (const auto& linkedSource : linkedSources) {
-                    linkCommand += " " + linkedSource.as<std::string>();
-                }
-
-                const YAML::Node& linkedLibraries = linkedStep["libraries"];
-                for (const auto& linkedLibrary : linkedLibraries) {
-                    linkCommand += " -l" + linkedLibrary.as<std::string>();
-                }
-
-                // Execute the link command
-                int linkResult = system(linkCommand.c_str());
-
-                if (linkResult != 0) {
-                    std::cerr << "Error: Failed to link with target '" << linkTargetName << "'\n";
-                    return;
-                }
-
-                std::cout << "Linking successful with target: " << linkTargetName << std::endl;
-            }
-        }
-
-        // Collecting source files
-        std::vector<std::string> sourceFiles;
-        for (const auto& source : sources) {
-            sourceFiles.push_back(source.as<std::string>());
-            std::cout << "Source: " << source.as<std::string>() << std::endl;
-        }
-
-        // Assuming "libraries" is a sequence of strings
-        const YAML::Node& libraries = step["libraries"];
-
-        // Collecting libraries
-        std::vector<std::string> libraryFiles;
-        for (const auto& library : libraries) {
-            libraryFiles.push_back(library.as<std::string>());
-            std::cout << "Libraries: " << library.as<std::string>() << std::endl;
-        }
-
-        // Add source files and libraries to the build command
-        for (const auto& source : sourceFiles) {
-            buildCommand += " " + source;
-        }
-
-        for (const auto& library : libraryFiles) {
-            buildCommand += " -l" + library;
-        }
-
-        std::ofstream installFile("install.amake");
-        if (!installFile.is_open()) {
-            std::cerr << "Error: Couldn't create install.amake\n";
-            return;
-        }
-
-        installFile << "install_targets:\n";
-
-        for (const auto& step : steps) {
-            std::string target = step["target"].as<std::string>();
-            bool install = step["install"].as<bool>();
-
-            if (install) {
-                installFile << "  - target: " << target << "\n";
-                installFile << "    type: " << step["type"].as<std::string>() << "\n";
-            }
-        }
-
-        installFile.close();
 
         // Execute the build command
         int result = system(buildCommand.c_str());
@@ -148,29 +73,32 @@ void buildProject() {
 
         std::cout << "Build successful for target: " << target << std::endl;
     }
-
-    // Add overall build logic if needed
 }
 
-void installProject()
-{
+// Function to install project using custom syntax
+void installProject() {
     std::ifstream installFile("install.amake");
     if (!installFile.is_open()) {
         std::cerr << "Error: install.amake not found\n";
         return;
     }
 
-    YAML::Node installConfig = YAML::Load(installFile);
-
-    const YAML::Node& installTargets = installConfig["install_targets"];
-    if (!installTargets.IsSequence()) {
-        std::cerr << "Error: 'install_targets' must be a sequence in install.amake\n";
-        return;
+    std::vector<std::string> installConfig;
+    for (std::string line; getline(installFile, line); ) {
+        installConfig.push_back(line);
     }
 
-    for (const auto& installTarget : installTargets) {
-        std::string target = installTarget["target"].as<std::string>();
-        std::string type = installTarget["type"].as<std::string>();
+    for (const auto& installTarget : installConfig) {
+        // Split the install target by space
+        std::vector<std::string> tokens = split(installTarget);
+
+        if (tokens.size() < 3) {
+            std::cerr << "Error: Invalid install target format\n";
+            return;
+        }
+
+        std::string target = tokens[2];
+        std::string type = tokens[4];
 
         // Implement installation logic based on target type
         if (type == "executable") {
@@ -180,11 +108,12 @@ void installProject()
         } else if (type == "shared" || type == "static") {
             // Install library to /usr/lib/
             const char* extension;
-            if(type == "shared")
+            if (type == "shared") {
                 int installResult = system(("sudo cp build/target/" + target + ".so /usr/bin/").c_str());
-            else
+            } else {
                 int installResult = system(("sudo cp build/target/" + target + ".a /usr/bin/").c_str());
-            
+            }
+
             // Check installResult for errors
         } else {
             std::cerr << "Error: Unknown target type '" << type << "' for target '" << target << "'\n";
